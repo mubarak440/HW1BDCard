f6b500ca0028003ec55f9ad8a2d266094ac114e8
15189 <15189@DESKTOP-CVVJR6M>
2022-05-04 07:10:19 -0400

    Add project files.

---
+++ Models/CardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BirthdayCardGenerator.Models
+{
+    public class CardViewModel
+    {
+        [Required(ErrorMessage = "Please enter From")]
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Please enter To")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Message")]
+        public string Message { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BirthdayCardGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
